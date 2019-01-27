cfc644a789b6a0f1490c6c56e1ec05c29087008d
Enzo Ferey <hello@enzoferey.com>
2019-01-27 14:05:27 +0100

    Coverage setup.

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,15 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   roots: ["<rootDir>/modules/"],
+  collectCoverage: true,
+  collectCoverageFrom: ["./modules/**/*.ts", "!./modules/**/__fixtures__/*"],
+  coverageDirectory: "./coverage/",
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
