58c731abcc2c7a0d181d157cfe4b57c89e8f4ec7
Enzo Ferey <hello@enzoferey.com>
2019-02-17 12:40:44 +0100

    Update build.

--- dist/cart/cart.js
+++ dist/cart/cart.js
@@ -1,81 +1,56 @@
 export function addToCart(items, item) {
-  if (item.quantity <= 0) {
+  const validItem = item.quantity <= 0;
+
+  if (validItem) {
     return items;
   }
 
   const itemIndex = getItemIndexById(items, item.id);
+  const itemAlreadyExists = itemIndex >= 0;
 
-  if (itemIndex >= 0) {
+  if (itemAlreadyExists) {
     // Increment quantity
     const currentQuantity = items[itemIndex].quantity;
     const targetQuantity = currentQuantity + item.quantity;
-    return replaceQuantityInCart(items, itemIndex, targetQuantity);
-  } else {
-    return addNewItemToCart(items, item);
-  }
-}
-export function getItemIndexById(items, id) {
-  return items.findIndex(item => item.id === id);
-}
-export function addNewItemToCart(items, item) {
-  if (item.quantity <= 0) {
-    return items;
-  } else {
-    const itemIndex = getItemIndexById(items, item.id);
+    return changeQuantityInCart(items, item.id, targetQuantity);
+  } // Add new item
 
-    if (itemIndex >= 0) {
-      return addToCart(items, item);
-    }
-  }
 
   return [...items, item];
 }
-export function removeFromCart(items, item) {
-  if (item.quantity <= 0) {
-    return items;
-  }
-
-  const itemIndex = getItemIndexById(items, item.id);
-
-  if (itemIndex < 0) {
-    return items;
-  } // Decrement quantity
-
-
-  const currentQuantity = items[itemIndex].quantity;
-  const targetQuantity = currentQuantity - item.quantity;
-
-  if (targetQuantity <= 0) {
-    return fullyRemoveItemFromCart(items, item.id);
-  }
-
-  return replaceQuantityInCart(items, itemIndex, targetQuantity);
-}
-export function fullyRemoveItemFromCart(items, id) {
+export function removeFromCart(items, id) {
   return items.filter(item => item.id !== id);
 }
-export function setQuantityInCart(items, id, quantity) {
+export function changeQuantityInCart(items, id, quantity) {
   if (quantity < 0) {
     return items;
   } else if (quantity === 0) {
-    return fullyRemoveItemFromCart(items, id);
+    return removeFromCart(items, id);
   }
 
-  const itemIndex = getItemIndexById(items, id);
-  return replaceQuantityInCart(items, itemIndex, quantity);
-}
-export function replaceQuantityInCart(items, index, quantity) {
+  const index = getItemIndexById(items, id);
+
+  if (index < 0) {
+    return items;
+  }
+
   const currentItem = items[index];
   const newItem = { ...currentItem,
     quantity
   };
-  const cart = [...items];
-  cart[index] = newItem;
-  return cart;
+  return replaceItemAtIndexInCart(items, index, newItem);
 }
 export function getTotalPriceCart(items) {
   return items.reduce((total, item) => total + item.price * item.quantity, 0);
 }
 export function getTotalNumberItemsCart(items) {
   return items.reduce((count, item) => count + item.quantity, 0);
+}
+export function getItemIndexById(items, id) {
+  return items.findIndex(item => item.id === id);
+}
+export function replaceItemAtIndexInCart(items, index, item) {
+  const cart = [...items];
+  cart[index] = item;
+  return cart;
 }
